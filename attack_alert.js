3b475fcde2bf2663ab12293fff15272c45ec6997
dedol <dedol@ro.ru>
2021-11-22 17:01:57 +0400

    change tab to space

--- attack_alert.js
+++ attack_alert.js
@@ -1,19 +1,19 @@
 let audio = new Audio('http://dedol.ru/alarm.mp3'); 
 
 function getEvents() {
-	return fetch("/game/index.php?page=componentOnly&component=eventList&action=fetchEventBox&ajax=1&asJson=1", {
-		"headers": {
-			"X-Requested-With": "XMLHttpRequest",
-		},
-		"method": "GET",
-	}).then(response => response.json());
+  return fetch("/game/index.php?page=componentOnly&component=eventList&action=fetchEventBox&ajax=1&asJson=1", {
+    "headers": {
+      "X-Requested-With": "XMLHttpRequest",
+    },
+    "method": "GET",
+  }).then(response => response.json());
 }
 
 function checkEvents() {
-	getEvents().then((data) => {
-		if (data.hostile != 0)
-			audio.play();
-	});
+  getEvents().then((data) => {
+    if (data.hostile != 0)
+      audio.play();
+  });
 }
 
 setInterval(() => {checkEvents()}, 60000);
